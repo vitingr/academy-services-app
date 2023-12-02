446e36310b899e403b85b33d84ac5da6b29827d1
Vitor Gabriel <vitorgabrielsbo1460@gmail.com>
2023-12-02 13:09:46 -0300

    Services update

--- components/Service.tsx
+++ components/Service.tsx
@@ -33,6 +33,35 @@ const Service = ({ content, handleFunction }: ServicePageProps) => {
     }
   }
 
+  const removeService = async () => {
+    try {
+      let servicesSubscribers = (content.subscribers).split(" ")
+      let indice = servicesSubscribers.indexOf(data._id)
+      servicesSubscribers.splice(indice, 1)
+      let newServiceSubscribers = servicesSubscribers.join(" ")
+
+      const response = await fetch("http://localhost:3030/services/update", {
+        method: "POST",
+        body: JSON.stringify({
+          Id: content._id,
+          Description: content.description,
+          Price: Number(content.price),
+          Subscribers: newServiceSubscribers,
+          Title: content.title,
+        })
+      })
+      if (response.ok) {
+        toast.success("Você assinou o serviço")
+        handleFunction()
+      } else {
+        toast.error("ERRO! Não foi possível assinar o serviço")
+      }
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className='p-6 border border-neutral-200 rounded-lg w-[300px]'>
       <ToastMessage />
@@ -45,9 +74,9 @@ const Service = ({ content, handleFunction }: ServicePageProps) => {
       <p className='mt-4 text-[#717171] text-sm text-justify'>{content.description} Lorem, ipsum dolor sit amet consectetur adipisicing elit. In ipsam magnam magni nemo, tenetur officia amet veritatis, nobis repellendus velit voluptates dolore architecto. Cum deserunt ipsum impedit quos consequuntur animi?</p>
       <p className='text-sm mt-6 flex w-full justify-between text-[#717171]'>Preço <span>{content.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</span></p>
       {content.subscribers.includes(data._id) ? (
-        <p className='text-[#F2921D] mt-10 w-full text-center'>
-          Você possui esse serviço
-        </p>
+        <button className='text-[#F2921D] mt-10 p-2 w-full text-center' onClick={() => removeService()}>
+          Cancelar Serviço
+        </button>
       ) : (
         <button className='text-white bg-[#F2921D] rounded-xl selection:bg-transparent p-2 mt-10 w-full' onClick={() => signService()}> 
           Assinar Serviço
